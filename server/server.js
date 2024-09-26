c83b000896e437b70712dad0faa751968bda92ff
George Croucher <gpcroucher@gmail.com>
2024-09-26 14:06:19 +0100

    added /pets GET endpoint to return all pets

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ const db = new pg.Pool({
   connectionString: process.env.DB_CONN_STRING,
 });
 
+app.get("/pets", async (request, response) => {
+  const dbResult = await db.query(`
+    SELECT pets.name AS petname, owners.name AS ownername, species.name AS species
+    FROM week07_assignment_pets AS pets
+    INNER JOIN week07_assignment_pet_owners AS owners ON owner_id = owners.id
+    INNER JOIN week07_assignment_pet_species AS species ON species_id = species.id;`);
+  response.json(dbResult.rows);
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080.");
 });
