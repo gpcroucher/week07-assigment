63a9068186f551475a7e34b4fd1516173299da86
George Croucher <gpcroucher@gmail.com>
2024-09-28 11:45:15 +0100

    viewpets now working

--- client/src/pages/ViewPets.jsx
+++ client/src/pages/ViewPets.jsx
@@ -1,22 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
 export default function ViewPets() {
+  const [pets, setPets] = useState([
+    { petname: "Obi", ownername: "Tim", species: "dog" },
+  ]);
+
   useEffect(() => {
     fetchPets();
+    return; // cleanup function?
+
+    async function fetchPets() {
+      console.log("Fetching pets...");
+      const petsData = await fetch(`http://localhost:8080/pets`);
+      const newPets = await petsData.json();
+      // return pets;
+      setPets(await newPets);
+    }
   }, []);
 
   return (
     <>
       <NavBar exclude={["/view"]} />
       <p>Hello!</p>
-      {/* pets here */}
+      {pets.map((pet, index) => {
+        console.log("Attempted to map pets");
+        return (
+          <div key={index}>
+            <p>
+              {pet.petname} is a {pet.species} who belongs to {pet.ownername}
+            </p>
+          </div>
+        );
+      })}
     </>
   );
-
-  async function fetchPets() {
-    const petsData = await fetch(`http://localhost:8080/pets`);
-    const pets = await petsData.json();
-    return pets;
-  }
 }
